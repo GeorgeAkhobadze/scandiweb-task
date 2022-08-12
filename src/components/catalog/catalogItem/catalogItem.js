202bec60afb32169553b266f569a380341ae39e3
axobadzegiorgi5 <axobadzegiorgi5@gmail.com>
2022-08-12 18:09:21 +0400

    cart add button

--- src/components/catalog/catalogItem/catalogItem.js
+++ src/components/catalog/catalogItem/catalogItem.js
@@ -1,12 +1,14 @@
  import CatalogImage from "./catalogitemimg.png"
+ import CartIcon from "./cart-icon.svg"
  import React from "react"
 import "./catalogitem.css"
 const CatalogItem = ({name, price, image}) => {
     return(
       
         <div className="catalogitem">
+            <img className="catalog-add-btn" src={CartIcon} alt="Add to Cart"/>
                         <div className="catalogitem-img">
-                <img src={image} alt="Product Name"/>
+                <img src={image} alt="Product Name"/>   
             </div>
             <a className="catalogitem-title">{name}</a>
             <a className="catalogitem-price">$50.00</a>
